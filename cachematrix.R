e40c247d25cacf3255b05543de58b70bc8799df9
Kadrocan <kadrocan@hotmail.com>
2015-10-25 17:28:21 +0200

    descriptions are added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,35 @@
-matrix( c(1,2,3,4),2,2)## Put comments here that give an overall description of what your
-## functions do
+## These funtions are for caching inverse of a matix which is a potentially
+## time-consuming computation. Instead of computing twice, they cache the result
+## and use it if matrix is same and inverse is already computed.
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-    cachedInverse <- NULL
-    set <- function(y) {
-        x <<- y
-        cachedInverse <<- NULL
+        cachedInverse <- NULL
+        set <- function(y) {
+            x <<- y
+            cachedInverse <<- NULL
     }
-    get <- function() x
-    setInverse <- function(inverse) cachedInverse <<- inverse
-    getInverse <- function() cachedInverse
-    list(set = set, get = get,
-    setInverse = setInverse,
-    getInverse = getInverse)
+        get <- function() x
+        setInverse <- function(inverse) cachedInverse <<- inverse
+        getInverse <- function() cachedInverse
+        list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
-## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
-    invFunc <- x$getInverse()
-    if(!is.null(invFunc)) {
-        message("getting cached data")
-        return(invFunc)
+        invFunc <- x$getInverse()
+        if(!is.null(invFunc)) {
+            message("getting cached data")
+            return(invFunc)
     }
-    data <- x$get()
-    invFunc <- solve(data, ...)
-    x$setInverse(invFunc)
-    invFunc
+        data <- x$get()
+        invFunc <- solve(data, ...)
+        x$setInverse(invFunc)
+        invFunc
 }
